b795c1716c3d7d300601e2580008dcd5939ef02f
jobmwitah <jobmakuli042@gmail.com>
2025-03-14 11:38:30 +0300

    Update Tests

--- test/v1/Base_TestV1.t.sol
+++ test/v1/Base_TestV1.t.sol
@@ -21,12 +21,13 @@ pragma solidity 0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {IHorseStore} from "../../src/horseStoreV1/interfaces/IHorseStore.sol";
 
 abstract contract Base_TestV1 is Test {
-    HorseStore public horseStore;
+    IHorseStore public horseStore;
 
     function setUp() public virtual {
-        horseStore = new HorseStore();
+        horseStore = IHorseStore(address(new HorseStore()));
     }
 
     function testReadValue() public {
--- test/v1/HorseStorePureYul.t.sol
+++ test/v1/HorseStorePureYul.t.sol
@@ -1,13 +1,13 @@
 //SPDX-Licence-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
+import {Base_TestV1, IHorseStore} from "./Base_TestV1.t.sol";
 import {YulDeployer} from "deploy-yul/YulDeployer.sol";
 
-contract HorseStoreYul is Base_TestV1 {
+contract HorseStorePureYul is Base_TestV1 {
     YulDeployer yulDeployer = new YulDeployer();
 
     function setUp() public override {
-        horseStore = HorseStore(yulDeployer.deployContract("HorseStoreYul"));
+        horseStore = IHorseStore(yulDeployer.deployContract("HorseStoreYul"));
     }
 }
