f7c36652b65f4be2117ab65e6297fc0a4987e725
jobmwitah <jobmakuli042@gmail.com>
2025-03-14 10:40:34 +0300

    Write Differential Tests

---
+++ test/v1/HorseStoreYul.t.sol
@@ -0,0 +1,13 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
+import {YulDeployer} from "../lib/YulDeployer.sol";
+
+contract HorseStoreYul is Base_TestV1 {
+    YulDeployer yulDeployer = new YulDeployer();
+
+    function setUp() public override {
+        horseStore = HorseStore(yulDeployer.deployContract("HorseStoreYul"));
+    }
+}
